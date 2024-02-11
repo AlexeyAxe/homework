a55bd47ed0a8817db0aa79f032279561a11b0f9d
Alexey Aksionov <alex_331@mail.ru>
2024-02-11 22:10:25 +0300

    Last version

--- MyArrayList/src/Main.java
+++ MyArrayList/src/Main.java
@@ -1,27 +1,26 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    MyNewArrayClass array=new MyNewArrayClass();
-    Object o1= new Object();
-    System.out.println(array.isEmpty());
-    array.add("Александр");
-    array.add("Ольга");
-    array.add("Ксения");
-    array.add("Александр");
-    System.out.println(array.size());
-    System.out.println(array.get(1));
-    ArrayList<Object> list=new ArrayList<>();
-        {{
-            list.add("12");
-            list.add("22");
-            list.add("32");
-            list.add("42");
-        }}
-    array.addAll(1,list);
+        MyNewArrayClass array = new MyNewArrayClass();
+        Object o1 = new Object();
+        System.out.println(array.isEmpty());
+        array.add("Александр");
+        array.add("Ольга");
+        array.add("Ксения");
+        array.add("Александр");
+        System.out.println(array.size());
+        System.out.println(array.get(1));
+        ArrayList<Object> list = new ArrayList<>();
+        {
+            {
+                list.add("12");
+                list.add("22");
+                list.add("32");
+                list.add("42");
+            }
+        }
+        array.addAll(1, list);
         array.remove("Александр");
         array.sortedArray();
         array.print();
--- MyArrayList/src/MyNewArrayClass.java
+++ MyArrayList/src/MyNewArrayClass.java
@@ -1,112 +1,125 @@
 import java.util.Collection;
 
 public class MyNewArrayClass {
-    public Object[]array;
+    public Object[] array;
     private int size;
     private int capacity;
-     public MyNewArrayClass(){
-         capacity=10;
-         size=0;
-         array=new Object[capacity];
-     }
-     private void grow(){
-         capacity =(int)(capacity*1.5+1);
-         Object []arrayClone= new Object[capacity];
-        for(int i=0;i<array.length;i++){
-            arrayClone[i]=array[i];
+
+    public MyNewArrayClass() {
+        capacity = 10;
+        size = 0;
+        array = new Object[capacity];
+    }
+
+    private void grow() {
+        capacity = (int) (capacity * 1.5 + 1);
+        Object[] arrayClone = new Object[capacity];
+        for (int i = 0; i < array.length; i++) {
+            arrayClone[i] = array[i];
         }
         array = arrayClone;
 
 
-     }
-     public Object get(int index){
-         return array[index];
-     }
-     public Object remove(int index){
-         Object obj=array[index];
-         for(int i=index;i< array.length-1;i++){
-             array[i]=array[i+1];
-         }
-         array[array.length-1]=null;
-         size--;
-         return obj;
-     }
-    public boolean remove(Object obj){
-         for(int i=0;i< size;i++){
-            if(obj.equals(array[i])) {
-                 for(int j=i;j< size-1;j++){
-                     array[j]=array[j+1];
-                 }
-                 array[size-1]=null;
-                 size--;
-                 return true;
+    }
+
+    public Object get(int index) {
+        return array[index];
+    }
+
+    public Object remove(int index) {
+        Object obj = array[index];
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = null;
+        size--;
+        return obj;
+    }
+
+    public boolean remove(Object obj) {
+        for (int i = 0; i < size; i++) {
+            if (obj.equals(array[i])) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[size - 1] = null;
+                size--;
+                return true;
             }
         }
         return false;
-     }
-     public void add(Object obj){
-         if(size==capacity){
-             grow();
-         }
-         array[size]=obj;
-         size++;
-     }
-    public void add(int index, Object obj){
-        if(size==capacity){
-            grow();
-        }
-        for (int i=size;i>=index;i--) {
-            array[i+1]=array[i];
-        }
-        array[index]=obj;
     }
-    public boolean addAll(Collection<Object> col){
-         while((size+ col.size())>capacity){
+
+    public void add(Object obj) {
+        if (size == capacity) {
             grow();
         }
-       for(Object o:col){
-           array[size]=o;
-           size++;
-       }
-       return true;
+        array[size] = obj;
+        size++;
     }
-    public boolean addAll(int index, Collection<Object> col){
-        while((size+ col.size())>capacity){
+
+    public void add(int index, Object obj) {
+        if (size == capacity) {
             grow();
         }
-        for(int i=size-1;i>=index;i--){
-            array[i+ col.size()]=array[i];
+        for (int i = size; i >= index; i--) {
+            array[i + 1] = array[i];
         }
-        for(Object o:col){
-            array[index]=o;
+        array[index] = obj;
+    }
+
+    public boolean addAll(Collection<Object> col) {
+        while ((size + col.size()) > capacity) {
+            grow();
+        }
+        for (Object o : col) {
+            array[size] = o;
+            size++;
+        }
+        return true;
+    }
+
+    public boolean addAll(int index, Collection<Object> col) {
+        while ((size + col.size()) > capacity) {
+            grow();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            array[i + col.size()] = array[i];
+        }
+        for (Object o : col) {
+            array[index] = o;
             index++;
             size++;
         }
         return true;
     }
-    public int size(){
-         return size;
+
+    public int size() {
+        return size;
     }
-    public boolean isEmpty(){
-        return size==0;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public void sortedArray(){
-      boolean isSorted =false;
-      while(!isSorted){
-          isSorted = true;
-          for(int i =0; i < size-1;i++){
-              if(((Comparable)array[i]).compareTo(array[i+1])>0){
-                  isSorted = false;
-                  Object temp = array[i];
-                  array[i]=array[i+1];
-                  array[i+1]=temp;
-              }
-          }
-      }
+
+    public void sortedArray() {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < size - 1; i++) {
+                if (((Comparable) array[i]).compareTo(array[i + 1]) > 0) {
+                    isSorted = false;
+                    Object temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+        }
     }
-    public void print(){
-        for(int i=0;i<size;i++){
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
             System.out.println(array[i].toString());
         }
     }
- }
+}
